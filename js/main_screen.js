a0767c4fc31d23444c2727f6dcc698158e26e67f
IttaloSantos <ittalosantoss@gmail.com>
2023-11-07 21:26:18 -0300

    Add method to play audio

--- js/main_screen.js
+++ js/main_screen.js
@@ -1,9 +1,12 @@
 let counter      = 0;
 let counter_year = document.getElementById("counter");
+const audio      = document.getElementById("audioContainer");
 
 function addYear()
 {
     console.log("Button has been clicked!");
     counter++;
     counter_year.textContent = counter;
+    
+    if(counter == 3) audio.play();
 }
